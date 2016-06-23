0aa794eef471f91503c7bc2711f01bd62cb97cc8
Yoshua Wuyts <i@yoshuawuyts.com>
2016-06-23 15:05:01 +0200

    test: use arrow functions

--- test.js
+++ test.js
@@ -10,170 +10,142 @@ test('should assert input types', function (t) {
 
 test('should route paths', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/', function (params) {
-        t.pass('called')
-      }),
-      route('/foo', noop)
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/', function (params) {
+      t.pass('called')
+    }),
+    route('/foo', noop)
+  ])
 
   router('/')
 })
 
 test('should match a default path', function (t) {
   t.plan(1)
-  const router = sheetRouter('/404', function (route) {
-    return [
-      route('/404', function (params) {
-        t.pass('called')
-      }),
-      route('/', noop)
-    ]
-  })
+  const router = sheetRouter('/404', (route) => [
+    route('/404', () => t.pass('called')),
+    route('/', noop)
+  ])
 
   router('/foo')
 })
 
 test('should return a value', function (t) {
   t.plan(1)
-  const router = sheetRouter('/404', function (route) {
-    return [
-      route('/404', function (params) {
-        return 'foo'
-      })
-    ]
-  })
+  const router = sheetRouter('/404', (route) => [
+    route('/404', (params) => 'foo')
+  ])
 
   t.equal(router('/foo'), 'foo', 'returned value')
 })
 
 test('should clean urls before matching', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', function (params) {
-        t.pass('called')
-      })
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/foo', (params) => t.pass('called'))
+  ])
 
   router('https://foobar.com/foo#hello-world?bar=baz')
 })
 
 test('should deliver arbitrary objects', function (t) {
   t.plan(2)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/', function (params, foo, bar) {
-        t.equal(foo, 'foo', 'is foo')
-        t.equal(bar, 'bar', 'is bar')
-      }),
-      route('/foo', noop)
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/', (params, foo, bar) => {
+      t.equal(foo, 'foo', 'is foo')
+      t.equal(bar, 'bar', 'is bar')
+    }),
+    route('/foo', noop)
+  ])
 
   router('/', 'foo', 'bar')
 })
 
 test('should route nested paths 2 levels deep', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/bar', function () {
-          t.pass('called')
-        })
-      ])
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/bar', function () {
+        t.pass('called')
+      })
+    ])
+  ])
 
   router('/foo/bar')
 })
 
 test('should route vanilla paths 2 levels deep', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/', function () {
-          t.pass('called')
-        })
-      ])
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/', function () {
+        t.pass('called')
+      })
+    ])
+  ])
 
   router('/foo')
 })
 
 test('should route nested paths 3 levels deep', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/baz', [
-          route('/bar', function () {
-            t.pass('called')
-          })
-        ])
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/baz', [
+        route('/bar', function () {
+          t.pass('called')
+        })
       ])
-    ]
-  })
+    ])
+  ])
 
   router('/foo/baz/bar')
 })
 
 test('should route cojoined paths 3 levels deep', function (t) {
   t.plan(1)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/baz/ban', [
-          route('/bar', function () {
-            t.pass('called')
-          })
-        ])
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/baz/ban', [
+        route('/bar', function () {
+          t.pass('called')
+        })
       ])
-    ]
-  })
+    ])
+  ])
 
   router('/foo/baz/ban/bar')
 })
 
 test('should route partial cojoined paths 3 levels deep', function (t) {
   t.plan(2)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/baz/:barp', [
-          route('/bar', function (params) {
-            t.equal(params.barp, 'ban', 'params match')
-            t.pass('called')
-          })
-        ])
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/baz/:barp', [
+        route('/bar', function (params) {
+          t.equal(params.barp, 'ban', 'params match')
+          t.pass('called')
+        })
       ])
-    ]
-  })
+    ])
+  ])
 
   router('/foo/baz/ban/bar')
 })
 
 test('should route multiple partials', function (t) {
   t.plan(3)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/', function () {
-        t.pass('called')
-      }),
-      route('/:foo', [
-        route('/:bar', function (params) {
-          t.equal(params.foo, 'first', 'params foo match')
-          t.equal(params.bar, 'second', 'params bar match')
-        })
-      ])
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/', function () {
+      t.pass('called')
+    }),
+    route('/:foo', [
+      route('/:bar', function (params) {
+        t.equal(params.foo, 'first', 'params foo match')
+        t.equal(params.bar, 'second', 'params bar match')
+      })
+    ])
+  ])
 
   router('/')
   router('/first/second')
@@ -181,18 +153,16 @@ test('should route multiple partials', function (t) {
 
 test('should route multiple nested paths 2 levels deep', function (t) {
   t.plan(2)
-  const router = sheetRouter(function (route) {
-    return [
-      route('/foo', [
-        route('/bar', function () {
-          t.pass('called')
-        }),
-        route('/baz', function () {
-          t.pass('called')
-        })
-      ])
-    ]
-  })
+  const router = sheetRouter((route) => [
+    route('/foo', [
+      route('/bar', function () {
+        t.pass('called')
+      }),
+      route('/baz', function () {
+        t.pass('called')
+      })
+    ])
+  ])
 
   router('/foo/bar')
   router('/foo/baz')
