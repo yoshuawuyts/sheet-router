6fe406c6559c6ffea27f8709962df1a599ee11b8
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-08-29 23:13:02 +0300

    Improve babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+// rollup does not have unassert support for now, so use the browserify transform
 import unassertify from 'unassertify'
 import browserifyPlugin from 'rollup-plugin-browserify-transform'
 
@@ -10,8 +11,10 @@ export default {
   plugins: [
     browserifyPlugin(unassertify),
     nodeResolve({jsnext: true, main: true}),
-    commonjs({ include: 'node_modules/**' }),
-    babel()
+    commonjs(),
+    babel({
+      include: ['node_modules/wayfarer/**', 'node_modules/xtend', 'src/**']
+    })
   ],
   moduleName: 'sheetRouter',
   dest: 'dist/bundle.js'
