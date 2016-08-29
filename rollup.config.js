818b5b557d316d3c5fd93c841b482f0b633eb533
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-08-29 21:23:17 +0300

    Use unassertify to remove assertions
    There is no unassert plugin for browserify yet, so use the browserify
    transform

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import babel from 'rollup-plugin-babel'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import strip from 'rollup-plugin-strip'
+import unassertify from 'unassertify'
+import browserifyPlugin from 'rollup-plugin-browserify-transform'
 
 export default {
   entry: 'src/index.js',
   format: 'umd',
   plugins: [
+    browserifyPlugin(unassertify),
     nodeResolve({jsnext: true, main: true}),
-    strip({functions: ['assert.*']}),
-    commonjs(),
+    commonjs({ include: 'node_modules/**' }),
     babel()
   ],
   moduleName: 'sheetRouter',
