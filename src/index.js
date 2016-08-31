f7084e937f72dc6d30931dbaa4de6b7b501a17c4
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2016-08-31 23:47:01 +0300

    Add missing history and hash function exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export {sheetRouter} from './router'
 export {createLocation} from './create-location'
+export {hash} from './hash'
+export {history} from './history'
 export {href} from './href'
 export {qs} from './qs'
 export {walk} from './walk'
