35bd9880d58e3aa6bfd397aad31f00eba864b6b3
Krawaller <info@krawaller.se>
2016-09-05 00:35:25 +0200

    href: add root arg
    * added tests and root functionality
    
    * slim non-catch event data
    
    * factor out good link definition
    
    * slimmed noncatchevents further
    
    * update signature in comment
    
    * corrected signature
    
    * correct documented return type
    
    * updated href documentation in readme

--- href.js
+++ href.js
@@ -8,15 +8,15 @@ const noRoutingAttrName = 'data-no-routing'
 // handle a click if is anchor tag with an href
 // and url lives on the same domain. Replaces
 // trailing '#' so empty links work as expected.
-// fn(str) -> null
-function href (cb) {
+// (fn(str), obj?) -> undefined
+function href (cb, root) {
   assert.equal(typeof cb, 'function', 'sheet-router/href: cb must be a function')
 
   window.onclick = function (e) {
     if ((e.button && e.button !== 0) || e.ctrlKey || e.metaKey || e.altKey || e.shiftKey) return
 
     const node = (function traverse (node) {
-      if (!node) return
+      if (!node || node === root) return
       if (node.localName !== 'a') return traverse(node.parentNode)
       if (node.href === undefined) return traverse(node.parentNode)
       if (window.location.host !== node.host) return traverse(node.parentNode)
---
+++ test/href.js
@@ -0,0 +1,103 @@
+const tape = require('tape')
+const href = require('../href')
+const window = require('global/window') // will be empty object shared with href
+const sinon = require('sinon')
+
+window.history = { pushState: sinon.spy() }
+window.location = {}
+
+const goodLink = { localName: 'a', href: 'someUrl#', hasAttribute: () => {} }
+
+const nonCatchEvents = {
+  'non-links': {
+    target: { localName: 'p' }
+  },
+  'link without href': {
+    target: { localName: 'a', hasAttribute: () => {} }
+  },
+  'link with data-no-routing': {
+    target: { localName: 'a', href: 'someUrl#', hasAttribute: (a) => a === 'data-no-routing' }
+  },
+  'event with ctrlKey': {
+    ctrlKey: true
+  },
+  'event with metaKey': {
+    metaKey: true
+  },
+  'event with altKey': {
+    altKey: true
+  },
+  'event with shiftKey': {
+    shiftKey: true
+  },
+  'button click': {
+    button: true
+  }
+}
+
+tape('href', (t) => {
+  t.test('should capture link click', (t) => {
+    t.plan(5)
+    const event = {
+      target: {
+        localName: 'somethingOtherThanALink',
+        parentNode: {
+          localName: 'stillNotALink',
+          parentNode: goodLink
+        }
+      },
+      preventDefault: sinon.spy()
+    }
+    const previousPushCount = window.history.pushState.callCount
+    const cb = sinon.spy()
+    href(cb)
+    window.onclick(event)
+
+    t.equal(event.preventDefault.callCount, 1)
+    t.equal(cb.callCount, 1)
+    t.equal(cb.lastCall.args[0], 'someUrl')
+    t.equal(window.history.pushState.callCount, previousPushCount + 1)
+    t.deepEqual(window.history.pushState.lastCall.args, [{}, null, 'someUrl'])
+  })
+
+  for (var nonCatchEvent in nonCatchEvents) {
+    t.test('should avoid ' + nonCatchEvent, (t) => {
+      t.plan(3)
+      const event = Object.assign({
+        target: goodLink,
+        preventDefault: sinon.spy()
+      }, nonCatchEvents[nonCatchEvent])
+      const previousPushCount = window.history.pushState.callCount
+      const cb = sinon.spy()
+      href(cb)
+      window.onclick(event)
+
+      t.equal(event.preventDefault.callCount, 0)
+      t.equal(cb.callCount, 0)
+      t.equal(window.history.pushState.callCount, previousPushCount)
+    })
+  }
+
+  t.test('should avoid link outside provided root', (t) => {
+    t.plan(3)
+    const event = {
+      target: {
+        localName: 'somethingOtherThanALink',
+        parentNode: {
+          localName: 'root',
+          parentNode: goodLink
+        }
+      },
+      preventDefault: sinon.spy()
+    }
+    const previousPushCount = window.history.pushState.callCount
+    const cb = sinon.spy()
+    const root = event.target.parentNode
+    href(cb, root)
+    window.onclick(event)
+
+    t.equal(event.preventDefault.callCount, 0)
+    t.equal(cb.callCount, 0)
+    t.equal(window.history.pushState.callCount, previousPushCount)
+  })
+})
