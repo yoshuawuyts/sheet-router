2862b6d04159f91323461d6c2e9df7c2b4214531
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-11-30 11:45:36 -0600

    fix(pathname): fix electron regex for non index.html
    Now it applies the electron fix for all .htm(l) filenames, not just index.html.
    
    Also fixed what appeared to be a typo in the regex.
    
    Fixes #66

--- test/_pathname.js
+++ test/_pathname.js
@@ -3,11 +3,12 @@ const test = require('tape')
 const p = require('../_pathname')
 
 test('pathname', (t) => {
-  t.plan(6)
+  t.plan(7)
   t.equal(p('https://foobar.com/bin/baz#bar'), 'bin/baz/bar')
   t.equal(p('http://foobar.com/bin/baz#bar'), 'bin/baz/bar')
   t.equal(p('http://foobar.com/bin/baz#bar?beep=boop'), 'bin/baz/bar')
   t.equal(p('http://foobar.com/bin/baz#bar?beep=boop'), 'bin/baz/bar')
   t.equal(p('/Users/dat/index.html/bin/baz', true), 'bin/baz')
   t.equal(p('file:///Users/anon/src/juliangruber/dat-desktop/index.html', true), '')
+  t.equal(p('file:///Users/anon/src/juliangruber/dat-desktop/app.html', true), '')
 })
